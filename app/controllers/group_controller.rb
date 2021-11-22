348c5ff927c04a4e62da7d85ebb9b42fd847ade0
d4nielj <d4niel.djm@gmail.com>
2021-11-22 12:33:33 -0500

    Add controllers

---
+++ app/controllers/group_controller.rb
@@ -0,0 +1,7 @@
+class GroupController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def create; end
+end
---
+++ app/controllers/report_controller.rb
@@ -0,0 +1,7 @@
+class ReportController < ApplicationController
+  def show; end
+
+  def new; end
+
+  def create; end
+end
