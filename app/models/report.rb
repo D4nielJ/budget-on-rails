58593212530d8a4e1c74544d85d8f43529e5a245
d4nielj <d4niel.djm@gmail.com>
2021-11-23 17:56:18 -0500

    Fix linter errors

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,6 @@
 class Report < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :groups
-  accepts_nested_attributes_for :groups
 
   validates_presence_of :name
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
