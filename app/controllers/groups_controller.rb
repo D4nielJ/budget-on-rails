07b184975866f4acaaeb4407556d19a02fc97b86
d4nielj <d4niel.djm@gmail.com>
2021-11-23 15:02:46 -0500

    Add create method for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,7 +6,26 @@ class GroupsController < ApplicationController
     @total = helpers.total_expenses_all
   end
 
-  def new; end
+  def new
+    @group = Group.new
+  end
 
-  def create; end
+  def create
+    user = current_user
+    group = user.groups.new(group_params)
+
+    if group.save
+      flash[:notice] = 'Categorie added successfully'
+      redirect_to groups_path
+    else
+      flash[:error] = group.errors.messages
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
