93c7cd99fccc036126e59cd2e063c9108b7a7d64
d4nielj <d4niel.djm@gmail.com>
2021-11-24 21:57:52 -0500

    Style alert notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,6 +19,16 @@ module ApplicationHelper
       text_color = 'text-indigo-100'
     end
 
+    if type == 'alert'
+      icon = '<path stroke-linecap="round"
+      stroke-linejoin="round" stroke-width="2"
+      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0
+       2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />'
+      icon_color = 'text-red-300'
+      bg_color = 'bg-red-900'
+      text_color = 'text-red-100'
+    end
+
     {
       icon: icon,
       icon_color: icon_color,
