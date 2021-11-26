df1ad9c15c9f28027aa29040dc2a905cdc20afcb
d4nielj <d4niel.djm@gmail.com>
2021-11-26 13:43:53 -0500

    Fix icons in views and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,16 +14,27 @@ User.create!(
   password_confirmation: 'password'
 )
 
-food = Group.create(name: 'Food', icon: '', user_id: user_one.id)
-bills = Group.create(name: 'Bills', icon: '', user_id: user_one.id)
-outing = Group.create(name: 'Outing', icon: '', user_id: user_one.id)
-travel = Group.create(name: 'Travel', icon: '', user_id: user_one.id)
+food = Group.create!(name: 'Food',
+                     icon: 'https://cdn-icons.flaticon.com/png/512/562/premium/562678.png?token=exp=1637953056~hmac=a71c47841abf96df216de38c9345a2d1',
+                     user_id: user_one.id)
 
-burger = Report.create(name: 'Burger', amount: 5, author_id: user_one.id)
-movie = Report.create(name: 'Movie', amount: 10, author_id: user_one.id)
-internet = Report.create(name: 'Internet', amount: 50, author_id: user_one.id)
-rent = Report.create(name: 'Rent', amount: 200, author_id: user_one.id)
-jamaica = Report.create(name: 'I went to Jamaica', amount: 10_000, author_id: user_one.id)
+bills = Group.create!(name: 'Bills',
+                      icon: 'https://cdn-icons-png.flaticon.com/512/1611/1611154.png',
+                      user_id: user_one.id)
+
+outing = Group.create!(name: 'Outing',
+                       icon: 'https://cdn-icons.flaticon.com/png/512/4488/premium/4488772.png?token=exp=1637952987~hmac=86d6c2a38b749410fc4f4c3b9242f4bc',
+                       user_id: user_one.id)
+
+travel = Group.create!(name: 'Travel',
+                       icon: 'https://cdn-icons.flaticon.com/png/512/870/premium/870092.png?token=exp=1637952940~hmac=2653091d4ee899e23d1debeec3b6d3e4',
+                       user_id: user_one.id)
+
+burger = Report.create!(name: 'Burger', amount: 5, author_id: user_one.id)
+movie = Report.create!(name: 'Movie', amount: 10, author_id: user_one.id)
+internet = Report.create!(name: 'Internet', amount: 50, author_id: user_one.id)
+rent = Report.create!(name: 'Rent', amount: 200, author_id: user_one.id)
+jamaica = Report.create!(name: 'I went to Jamaica', amount: 10_000, author_id: user_one.id)
 
 food.reports << burger
 bills.reports << internet
